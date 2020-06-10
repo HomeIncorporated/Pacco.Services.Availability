fb53bfb6b4501c3b7321ca6b5f3816f22fb1f6f5
GooRiOn <foreverframenet@gmail.com>
2020-06-10 11:53:01 +0200

    Added RabbitMq pub/sub

--- src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
+++ src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToMessageMapper.cs
@@ -1,10 +1,27 @@
 using System;
 using Convey.MessageBrokers.RabbitMQ;
+using Pacco.Services.Availability.Application.Commands;
+using Pacco.Services.Availability.Application.Events.Rejected;
+using Pacco.Services.Availability.Application.Exceptions;
+using Pacco.Services.Availability.Core.Exceptions;
 
 namespace Pacco.Services.Availability.Infrastructure.Exceptions
 {
     internal sealed class ExceptionToMessageMapper : IExceptionToMessageMapper
     {
-        public object Map(Exception exception, object message) => null;
+        public object Map(Exception exception, object message) 
+            => exception switch
+            {
+                MissingResourceTagsException ex => new AddResourceRejected(((AddResource)message).ResourceId, 
+                    ex.Code, ex.Message),
+                InvalidResourceTagsException ex => new AddResourceRejected(((AddResource)message).ResourceId, 
+                    ex.Code, ex.Message),
+                ResourceAlreadyExistsException ex => message switch
+                {
+                    AddResource cmd => new AddResourceRejected(cmd.ResourceId,ex.Code, ex.Message),
+                    _ => null
+                },
+                _ => null
+            };
     }
 }
--- src/Pacco.Services.Availability.Infrastructure/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Extensions.cs
@@ -7,6 +7,7 @@ using Convey.CQRS.Commands;
 using Convey.CQRS.Events;
 using Convey.CQRS.Queries;
 using Convey.MessageBrokers;
+using Convey.MessageBrokers.CQRS;
 using Convey.MessageBrokers.RabbitMQ;
 using Convey.Persistence.MongoDB;
 using Convey.Persistence.Redis;
@@ -15,10 +16,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
+using Pacco.Services.Availability.Application.Commands;
 using Pacco.Services.Availability.Application.Services;
 using Pacco.Services.Availability.Core.Repositories;
 using Pacco.Services.Availability.Infrastructure.Contexts;
-using Pacco.Services.Availability.Infrastructure.Decorators;
+using Pacco.Services.Availability.Application.Events.External;
 using Pacco.Services.Availability.Infrastructure.Exceptions;
 using Pacco.Services.Availability.Infrastructure.Logging;
 using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
@@ -45,6 +47,7 @@ namespace Pacco.Services.Availability.Infrastructure
                 .AddExceptionToMessageMapper<ExceptionToMessageMapper>()
                 .AddMongo()
                 .AddRedis()
+                .AddRabbitMq()
                 .AddHandlersLogging()
                 .AddMongoRepository<ResourceDocument, Guid>("resources");
         }
@@ -52,7 +55,11 @@ namespace Pacco.Services.Availability.Infrastructure
         public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
         {
             app.UseErrorHandler()
-                .UseConvey();
+                .UseConvey()
+                .UseRabbitMq()
+                .SubscribeCommand<AddResource>()
+                .SubscribeCommand<ReserveResource>()
+                .SubscribeEvent<SignedUp>();
 
             return app;
         }
