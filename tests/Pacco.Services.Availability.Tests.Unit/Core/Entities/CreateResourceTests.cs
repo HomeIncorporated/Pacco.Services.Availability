173313a35872d68d686161cd12fd6b59f34dbb3e
GooRiOn <foreverframenet@gmail.com>
2020-06-12 12:46:57 +0200

    Added unit tests for resource creation

---
+++ tests/Pacco.Services.Availability.Tests.Unit/Core/Entities/CreateResourceTests.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.Events;
+using Pacco.Services.Availability.Core.Exceptions;
+using Shouldly;
+using Xunit;
+
+namespace Pacco.Services.Availability.Tests.Unit.Core.Entities
+{
+    public class CreateResourceTests
+    {
+        Resource Act(AggregateId id, IEnumerable<string> tags)
+            => Resource.Create(id, tags);
+        
+        [Fact]
+        public void given_valid_id_and_tags_resource_should_be_created()
+        {
+            var id = new AggregateId();
+            var tags = new[] {"tag"};
+
+            var resource = Act(id, tags);
+            
+            resource.ShouldNotBeNull();
+            resource.Id.ShouldBe(id);
+            resource.Tags.ShouldBe(tags);
+            
+            resource.Events.Count().ShouldBe(1);
+            var domainEvent = resource.Events.First();
+            domainEvent.ShouldBeOfType<ResourceCreated>();
+        }
+
+        [Fact]
+        public void given_empty_tags_resource_should_throw_an_exception()
+        {
+            var id = new AggregateId();
+            var tags = Enumerable.Empty<string>();
+
+            var exception = Record.Exception(() => Act(id, tags));
+            
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<MissingResourceTagsException>();
+        }
+    }
+}
