d076d1c3c7851dd9afe845553b693686fe218644
GooRiOn <foreverframenet@gmail.com>
2020-06-12 13:10:10 +0200

    Added unit tests for both Core and Application layer

---
+++ tests/Pacco.Services.Availability.Tests.Unit/Application/Handlers/AddResourceHandlerTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading.Tasks;
+using NSubstitute;
+using Pacco.Services.Availability.Application.Commands;
+using Pacco.Services.Availability.Application.Commands.Handlers;
+using Pacco.Services.Availability.Application.Exceptions;
+using Pacco.Services.Availability.Application.Services;
+using Pacco.Services.Availability.Core.Entities;
+using Pacco.Services.Availability.Core.Repositories;
+using Shouldly;
+using Xunit;
+
+namespace Pacco.Services.Availability.Tests.Unit.Application.Handlers
+{
+    public class AddResourceHandlerTests
+    {
+        Task Act(AddResource command)
+            => _commandHandler.HandleAsync(command);
+
+        [Fact]
+        public async Task given_resource_id_that_already_exists_the_handler_should_throw_an_exception()
+        {
+            var command = new AddResource(Guid.NewGuid(), new []{"tags"});
+            _repository.ExistsAsync(command.ResourceId).Returns(true);
+
+            var exception = await Record.ExceptionAsync(() => Act(command));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<ResourceAlreadyExistsException>();
+        }
+
+        [Fact]
+        public async Task given_valid_id_and_tags_the_resource_should_be_persisted_using_repository()
+        {
+            var command = new AddResource(Guid.NewGuid(), new []{"tags"});
+            await Act(command);
+
+            await _repository
+                .Received(1)
+                .AddAsync(Arg.Is<Resource>(r => r.Id == command.ResourceId));
+        }
+        
+
+        #region ARRANGE
+
+        private readonly IResourcesRepository _repository;
+        private readonly IEventProcessor _eventProcessor;
+        private readonly AddResourceHandler _commandHandler;
+        
+        public AddResourceHandlerTests()
+        {
+            _repository = Substitute.For<IResourcesRepository>();
+            _eventProcessor = Substitute.For<IEventProcessor>();
+            _commandHandler = new AddResourceHandler(_repository, _eventProcessor);
+        }
+
+        #endregion
+    }
+}
--- tests/Pacco.Services.Availability.Tests.Unit/Core/Entities/CreateResourceTests.cs
+++ tests/Pacco.Services.Availability.Tests.Unit/Core/Entities/CreateResourceTests.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using Pacco.Services.Availability.Core.Entities;
 using Pacco.Services.Availability.Core.Events;
 using Pacco.Services.Availability.Core.Exceptions;
